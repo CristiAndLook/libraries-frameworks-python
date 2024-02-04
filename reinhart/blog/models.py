16c15be3c7156deda6a1775ac39b8f6069240038
CristiAndLook <krhistian1902@gmail.com>
2024-02-03 19:08:51 -0500

    Models, migrations and admin

--- reinhart/blog/models.py
+++ reinhart/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True) 
+    modified = models.DateTimeField(auto_now=True)

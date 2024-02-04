33305f165c5f6914e402cf416a054e0904162f60
CristiAndLook <krhistian1902@gmail.com>
2024-02-04 16:50:58 -0500

    Context and Template tags

--- reinhart/blog/views.py
+++ reinhart/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
